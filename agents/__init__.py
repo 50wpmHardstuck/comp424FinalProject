6295ced7326bc02352b818a3547797a44a8151c1
Denis Tsariov <tsariov.den@gmail.com>
2023-12-04 23:37:13 -0500

    best agent commit

--- agents/__init__.py
+++ agents/__init__.py
@@ -5,3 +5,4 @@ from .student_agent import StudentAgent
 from .new_agent import Random3Agent
 from .improved_agent import BetterStudent
 from .student_agent_copy import StudentAgent
+from .best_agent import StudentAgent
